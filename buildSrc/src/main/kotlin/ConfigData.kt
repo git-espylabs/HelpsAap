1d80b33101beced11728c1b365aa49460831dfae
DESKTOP-KB1SJEC\JOBSIN JOSEPH <jobsinjoseph.mopl@gmail.com>
2022-09-29 23:28:42 +0530

    changes from jobsin

--- buildSrc/src/main/kotlin/ConfigData.kt
+++ buildSrc/src/main/kotlin/ConfigData.kt
@@ -4,8 +4,8 @@ object ConfigData {
     const val buildToolsVersion = "30.0.3"
     const val minSdkVersion = 26
     const val targetSdkVersion = 32
-    const val versionCode = 2
-    const val versionName = "2.0"
+    const val versionCode = 3
+    const val versionName = "3.0"
     const val dimension = "environment"
     const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
